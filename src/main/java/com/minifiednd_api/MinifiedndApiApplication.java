fceff796879d3edaee4bc3006a511270fb46e462
Oleksiy Korniychuk <okorniychuk2@gmail.com>
2021-02-13 14:43:08 -0600

    Added global config for /flow2 CORS

--- src/main/java/com/minifiednd_api/MinifiedndApiApplication.java
+++ src/main/java/com/minifiednd_api/MinifiedndApiApplication.java
@@ -3,7 +3,10 @@ package com.minifiednd_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.minifiednd_api"})
@@ -13,4 +16,13 @@ public class MinifiedndApiApplication extends SpringBootServletInitializer {
 		SpringApplication.run(MinifiedndApiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/flow2").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
