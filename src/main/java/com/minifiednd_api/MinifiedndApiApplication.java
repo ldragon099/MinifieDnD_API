ba949a4ea994902edfb0cfb7149e58486a13fcc6
Oleksiy Korniychuk <okorniychuk2@gmail.com>
2021-02-13 21:35:29 -0600

    separated our the CORS mapping into /allBioms and /flow2

--- src/main/java/com/minifiednd_api/MinifiedndApiApplication.java
+++ src/main/java/com/minifiednd_api/MinifiedndApiApplication.java
@@ -21,7 +21,8 @@ public class MinifiedndApiApplication extends SpringBootServletInitializer {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000, http://minifiednd.com");
+				registry.addMapping("/allBioms").allowedOrigins("http://localhost:3000, http://minifiednd.com");
+				registry.addMapping("/flow2").allowedOrigins("http://localhost:3000, http://minifiednd.com");
 			}
 		};
 	}
