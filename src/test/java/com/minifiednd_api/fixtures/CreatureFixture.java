50435f5ee4c36cf62738809de75bb71364da1b4b
Oleksiy Korniychuk <okorniychuk2@gmail.com>
2021-03-22 23:25:48 -0500

    Added CreaturesFixture, and more RandomSubset unit tests. These may need to be combined in some way as checking so many cases can get tedious

---
+++ src/test/java/com/minifiednd_api/fixtures/CreatureFixture.java
@@ -0,0 +1,30 @@
+package com.minifiednd_api.fixtures;
+
+import com.minifiednd_api.models.Creature;
+
+import java.util.*;
+
+public class CreatureFixture {
+    public static Creature getCreature() {
+        return new Creature(0, "Cat", "mm 320", "Tiny", "Unaligned", "0");
+    }
+
+    public static Object getCreatureAsObject() {
+        Map<String, String> map;
+        map = new HashMap<>();
+        map.put("name", "Cat");
+        map.put("source", "mm 320");
+        map.put("size", "Tiny");
+        map.put("alignment", "Unaligned");
+        map.put("cr", "0");
+        return map;
+    }
+
+    public static List<Object> getListOfCreaturesAsObjects(Integer length) {
+        List<Object> list = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            list.add(getCreatureAsObject());
+        }
+        return list;
+    }
+}
--- src/test/java/com/minifiednd_api/services/CreatureServiceTests.java
+++ src/test/java/com/minifiednd_api/services/CreatureServiceTests.java
@@ -1,5 +1,7 @@
 package com.minifiednd_api.services;
 
+import com.minifiednd_api.fixtures.CreatureFixture;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.Assert;
@@ -10,6 +12,7 @@ import java.util.List;
 @SpringBootTest
 public class CreatureServiceTests {
     // When_StateUnderTest_Expect_ExpectedBehavior
+
     @Test
     void When_RandomSubsetIsPassedList_Expect_ReturnList() {
         // Arrange
@@ -18,21 +21,36 @@ public class CreatureServiceTests {
         // Act
         List<Object> subset = CreatureService.RandomSubset(list, subsetLength);
         // Assert
-        Assert.assertNotNull(subset);
+        Assertions.assertNotNull(subset);
     }
 
     // TODO: test index out of bound
 
-//    @Test
-//    void When_RandomSubsetIsPassedNullSubsetLength_Expect_ReturnSameListThatWasPassedIn() {
-//        // Arrange
-//        List<Object> inputList = Collections.emptyList();
-//        inputList.add(3);
-//        inputList.add(2);
-//        inputList.add(1);
-//        // Act
-//        List<Object> subsetList = CreatureService.RandomSubset(inputList, null);
-//        // Assert
-//        Assert.assertEquals("Arrays are not equal", inputList, subsetList);
-//    }
+    @Test
+    void When_RandomSubsetIsPassedNullSubsetLength_Expect_ReturnSameListThatWasPassedIn() {
+        // Arrange
+        List<Object> inputList = CreatureFixture.getListOfCreaturesAsObjects(3);
+        // Act
+        List<Object> subsetList = CreatureService.RandomSubset(inputList, null);
+        // Assert
+        Assertions.assertEquals(inputList, subsetList, "Arrays are not equal");
+    }
+
+    @Test
+    void When_RandomSubsetIsPassedValidSubsetLength_Expect_ReturnCorrectLengthSubst() {
+        // Arrange
+        List<Object> inputList = CreatureFixture.getListOfCreaturesAsObjects(5);
+        Integer subsetLength = 3;
+        // Act
+        List<Object> subsetList = CreatureService.RandomSubset(inputList, subsetLength);
+        // Assert
+        Assertions.assertEquals((long)subsetLength, subsetList.size(), "Subset array is not of the expected length");
+    }
+
+    @Test
+    void When_ConvertObjectToCreatureIsCalled_Expect_Something() {
+        // Arrange
+        // Act
+        // Assert
+    }
 }
